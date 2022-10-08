db246843733ab66ab3782bde35997ad9269ed271
yunmin <362279869@qq.com>
2022-10-08 23:42:41 +0800

    convert

--- src/handle.js
+++ src/handle.js
@@ -8,6 +8,7 @@ const {
     utils,
     fileSystem
 } = require("exp-utils");
+const localutils = require('./utils')
 const Portfolio = require('./portfolio');
 const configPath = "./config/config.json";
 
@@ -40,9 +41,25 @@ exports.setWritePath = function (params, next) {
 exports.startConvert = function (params, next) {
     console.log(params);
     let portfolio = Portfolio.create(config.readPath);
-    let names = portfolio.getDocumentsName();   
-    names.forEach(function (name) {    
-
+    let names = portfolio.getDocumentsName();
+    names.forEach(function (name) {
+        console.log('Converting document: ' + name);
+        let data = portfolio.doc2Object(name);
+        // simplified data
+        if (data) {
+            localutils.objectSimplify(data.nor);
+        }
+        // save to file
+        if (data) {
+            fileSystem.writeSync(config.writePath + name + '.json', data.nor);
+            if (data.rep.length > 0) {
+                console.warn('repeating field:');
+                console.warn(data.rep);
+            }
+            console.log('Exported document: ' + name);
+        } else {
+            console.warn('Invalid excel file: ' + name);
+        }
     });
     utils.invoke(next);
 };
