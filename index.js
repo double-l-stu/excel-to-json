4f56bff74157cf5518dd3e802843bf794af7a571
yunmin <362279869@qq.com>
2022-10-08 22:47:33 +0800

    read/write path setting functions

--- index.js
+++ index.js
@@ -5,8 +5,12 @@
  */
 
 const {
-    utils,
     cmdSystem
 } = require("exp-utils");
+const handle = require("./src/handle");
 
+cmdSystem.add("-r", handle.setReadPath, "Set read directory of excel files(eg: -r your-path).");
+cmdSystem.add("-w", handle.setWritePath, "Set export directory of json files(eg: -w your-path).");
+cmdSystem.add("-c", handle.startConvert, "Convert excel files to json files(eg: -c or -c file1 file2...).");
 
+cmdSystem.start(true);
---
+++ src/handle.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: yunmin
+ * @Email: 362279869@qq.com
+ * @Date: 2022-10-08 21:29:07
+ */
+
+const {
+    utils,
+    fileSystem
+} = require("exp-utils");
+const configPath = "./config/config.json";
+
+let config = fileSystem.readAsJsonSync(configPath);
+
+console.log(`Read Path: ${config.readPath}`);
+console.log(`Write Path: ${config.writePath}\n`);
+
+exports.setReadPath = function (params, next) {
+    if (params.length > 0) {
+        config.readPath = params[0];
+        fileSystem.writeSync(configPath, config);
+    }
+    else {
+        console.log("The right command: -r your-path");
+    }
+    utils.invoke(next);
+};
+
+exports.setWritePath = function (params, next) {
+    if (params.length > 0) {
+        config.writePath = params[0];
+        fileSystem.writeSync(configPath, config);
+    } else {
+        console.log("The right command: -w your-path");
+    }
+    utils.invoke(next);
+};
+
+exports.startConvert = function (params, next) {
+    
+
+    utils.invoke(next);
+};
